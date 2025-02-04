0985167771df09abef153b635020552946cffb45
Jean Figeuroa <jbster559@gmail.com>
2025-02-03 18:20:24 -0800

    Add project files.

---
+++ COMP003A.LectureActivity4-2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Author: Jean Bryant Figueroa
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Practice implementing methods in C#
+
+namespace COMP003A.LectureActivity4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static void GreetUser()
+        {
+           Console.WriteLine("Hello, World!");
+        }
+
+        static void CalculateSum()
+        {
+
+        }
+        static void DisplayArray()
+        {
+
+        }
+    }
+}
